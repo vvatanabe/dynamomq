316dcf5d64c6d47cd2e3ce0197ea1349ab934d57
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-09-06 05:28:57 +0900

    feat: implements appdata package

---
+++ appdata/shipment.go
@@ -0,0 +1,64 @@
+package appdata
+
+import (
+	"github.com/vvatanabe/go82f46979/model"
+)
+
+type Shipment struct {
+	ID         string            `json:"id" dynamodbav:"id"`
+	Data       *ShipmentData     `json:"data" dynamodbav:"data"`
+	SystemInfo *model.SystemInfo `json:"system_info" dynamodbav:"system_info"`
+}
+
+func NewShipment() *Shipment {
+	return &Shipment{
+		SystemInfo: model.NewSystemInfo(),
+		Data:       NewShipmentData(),
+	}
+}
+
+func NewShipmentWithID(id string) *Shipment {
+	if id == "" {
+		panic("Shipment ID cannot be null!")
+	}
+
+	return &Shipment{
+		ID:         id,
+		SystemInfo: model.NewSystemInfoWithID(id),
+		Data:       NewShipmentDataWithID(id),
+	}
+}
+
+func (s *Shipment) SetID(id string) {
+	if id == "" {
+		panic("Shipment ID cannot be null!")
+	}
+
+	s.ID = id
+	s.SystemInfo.ID = id
+	s.Data.ID = id
+}
+
+func (s *Shipment) MarkAsPartiallyConstructed() {
+	s.SystemInfo.Status = model.StatusEnumUnderConstruction
+}
+
+func (s *Shipment) MarkAsReadyForShipment() {
+	s.SystemInfo.Status = model.StatusEnumReadyToShip
+}
+
+func (s *Shipment) IsQueued() bool {
+	return s.SystemInfo.InQueue
+}
+
+func (s *Shipment) GetLastUpdatedTimestamp() string {
+	return s.SystemInfo.LastUpdatedTimestamp
+}
+
+func (s *Shipment) SetLastUpdatedTimestamp(timestamp string) {
+	s.SystemInfo.LastUpdatedTimestamp = timestamp
+}
+
+func (s *Shipment) ResetSystemInfo() {
+	s.SystemInfo = model.NewSystemInfoWithID(s.ID)
+}
---
+++ appdata/shipmentitem.go
@@ -0,0 +1,17 @@
+package appdata
+
+type ShipmentItem struct {
+	SKU    string `json:"SKU" dynamodbav:"SKU"`
+	Packed bool   `json:"is_packed" dynamodbav:"is_packed"`
+}
+
+func NewShipmentItem() *ShipmentItem {
+	return &ShipmentItem{}
+}
+
+func NewShipmentItemWithParams(sku string, isPacked bool) *ShipmentItem {
+	return &ShipmentItem{
+		SKU:    sku,
+		Packed: isPacked,
+	}
+}
