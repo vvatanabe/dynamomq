051cc284a9994b233204df1c40eed94211e749b4
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-09-16 13:03:38 +0900

    fix: remove ConfigurationFileName and rename DefaultShipmentTableName to DefaultTableName

--- constant/constant.go
+++ constant/constant.go
@@ -3,8 +3,7 @@ package constant
 const (
 	AwsRegionDefault           = "us-east-1"
 	AwsProfileDefault          = "default"
-	ConfigurationFileName      = "configuration.json"
-	DefaultShipmentTableName   = "Shipment"
+	DefaultTableName           = "Shipment"
 	QueueingIndexName          = "queueud-last_updated_timestamp-index"
 	DlqQueueingIndexName       = "dlq-last_updated_timestamp-index"
 	VisibilityTimeoutInMinutes = 1
