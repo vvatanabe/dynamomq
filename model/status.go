3cb39786fb62bb94025a0a720d1d2bff9636124a
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-09-21 15:00:15 +0900

    refactor: Removed unused status "None"

--- model/status.go
+++ model/status.go
@@ -3,7 +3,6 @@ package model
 type Status string
 
 const (
-	StatusNone               Status = "NONE"
 	StatusUnderConstruction  Status = "UNDER_CONSTRUCTION"
 	StatusReadyToShip        Status = "READY_TO_SHIP"
 	StatusProcessingShipment Status = "PROCESSING_SHIPMENT"
