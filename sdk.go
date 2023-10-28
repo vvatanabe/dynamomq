fe9b5a282ae4575cfed2a23f53e998c815bf2f6a
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-10-28 16:44:43 +0900

    refactotr: merge result.go and stats.go into sdk.go

--- sdk.go
+++ sdk.go
@@ -916,3 +916,25 @@ const (
 	QueueTypeStandard QueueType = "STANDARD"
 	QueueTypeDLQ      QueueType = "DLQ"
 )
+
+type Result struct {
+	ID                   string `json:"id"`
+	Status               Status `json:"status"`
+	LastUpdatedTimestamp string `json:"last_updated_timestamp"`
+	Version              int    `json:"version"`
+}
+
+// QueueStats represents the structure to store Queue depth statistics.
+type QueueStats struct {
+	First100IDsInQueue         []string `json:"first_100_IDs_in_queue"`
+	First100SelectedIDsInQueue []string `json:"first_100_selected_IDs_in_queue"`
+	TotalRecordsInQueue        int      `json:"total_records_in_queue"`
+	TotalRecordsInProcessing   int      `json:"total_records_in_queue_selected_for_processing"`
+	TotalRecordsNotStarted     int      `json:"total_records_in_queue_pending_for_processing"`
+}
+
+// DLQStats represents the structure to store DLQ depth statistics.
+type DLQStats struct {
+	First100IDsInQueue []string `json:"first_100_IDs_in_queue"`
+	TotalRecordsInDLQ  int      `json:"total_records_in_DLQ"`
+}
