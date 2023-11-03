f1c2e4ca280b0eb3515e4c251c97a3523fc8c5a8
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-03 15:53:07 +0900

    style: sort importsi in test

--- sdk_test.go
+++ sdk_test.go
@@ -10,8 +10,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/upsidr/dynamotest"
