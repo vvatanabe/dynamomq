b192f7b9f27299d8f335f77aff8e285896bb20d2
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:43:10 +0900

    refactor: refactor qstat command to use CommandFactory

--- internal/cmd/qstat.go
+++ internal/cmd/qstat.go
@@ -2,34 +2,35 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/vvatanabe/dynamomq"
 )
 
-var qstatCmd = &cobra.Command{
-	Use:   "qstat",
-	Short: "Retrieves the queue statistics",
-	Long:  `Retrieves the queue statistics.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			return fmt.Errorf("... %v\n", err)
-		}
-		stats, err := client.GetQueueStats(ctx, &dynamomq.GetQueueStatsInput{})
-		if err != nil {
-			return err
-		}
-		printMessageWithData("", stats)
-		return nil
-	},
+func (f CommandFactory) CreateQueueStatCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "qstat",
+		Short: "Retrieves the queue statistics",
+		Long:  `Retrieves the queue statistics.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
+			if err != nil {
+				return err
+			}
+			stats, err := client.GetQueueStats(ctx, &dynamomq.GetQueueStatsInput{})
+			if err != nil {
+				return err
+			}
+			printMessageWithData("", stats)
+			return nil
+		},
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(qstatCmd)
-	qstatCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	qstatCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	qstatCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
+	c := defaultCommandFactory.CreateQueueStatCommand(flgs)
+	setDefaultFlags(c, flgs)
+	c.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
+	rootCmd.AddCommand(c)
 }
