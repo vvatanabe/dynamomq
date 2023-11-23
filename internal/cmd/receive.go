f012b9415417e1fa477446b04a85699b539c27e9
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:44:07 +0900

    refactor: refactor receive command to use CommandFactory

--- internal/cmd/receive.go
+++ internal/cmd/receive.go
@@ -2,42 +2,35 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/vvatanabe/dynamomq"
 )
 
-var receiveCmd = &cobra.Command{
-	Use:   "receive",
-	Short: "Receive a message from the queue .. it will replace the current ID with the peeked one",
-	Long:  `Receive a message from the queue .. it will replace the current ID with the peeked one.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			printError(err)
-			return
-		}
-		rr, err := client.ReceiveMessage(ctx, &dynamomq.ReceiveMessageInput{})
-		if err != nil {
-			printError(fmt.Sprintf("ReceiveMessage has failed! message: %s", err))
-			return
-		}
-		printMessageWithData("", ReceiveResult{
-			Message: rr.PeekedMessageObject,
-		})
-		return
-	},
-}
-
-type ReceiveResult struct {
-	Message *dynamomq.Message[any] `json:"message"`
+func (f CommandFactory) CreatReceiveCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "receive",
+		Short: "Receive a message from the queue .. it will replace the current ID with the peeked one",
+		Long:  `Receive a message from the queue .. it will replace the current ID with the peeked one.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
+			if err != nil {
+				return err
+			}
+			rr, err := client.ReceiveMessage(ctx, &dynamomq.ReceiveMessageInput{})
+			if err != nil {
+				return err
+			}
+			printMessageWithData("", rr.PeekedMessageObject)
+			return nil
+		},
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(receiveCmd)
-	receiveCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	receiveCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	receiveCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
+	c := defaultCommandFactory.CreatReceiveCommand(flgs)
+	setDefaultFlags(c, flgs)
+	c.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
+	rootCmd.AddCommand(c)
 }
