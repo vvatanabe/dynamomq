d383e529896b502c6c46296382b3739af26233a7
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:41:58 +0900

    refactor: refactor get command to use CommandFactory

--- internal/cmd/get.go
+++ internal/cmd/get.go
@@ -7,40 +7,33 @@ import (
 	"github.com/vvatanabe/dynamomq"
 )
 
-var getCmd = &cobra.Command{
-	Use:   "get",
-	Short: "Get a message the application object from DynamoDB by app domain ID",
-	Long:  `Get a message the application object from DynamoDB by app domain ID.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			printError(err)
-			return
-		}
-		id := flgs.ID
-		retrieved, err := client.GetMessage(ctx, &dynamomq.GetMessageInput{
-			ID: id,
-		})
-		if err != nil {
-			printError(err)
-			return
-		}
-		printMessageWithData("", ReceiveResult{
-			Message: retrieved.Message,
-		})
-		return
-	},
-}
-
-type GetResult struct {
-	Message *dynamomq.Message[any] `json:"message"`
+func (f CommandFactory) CreateGetCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "get",
+		Short: "Get a message the application object from DynamoDB by app domain ID",
+		Long:  `Get a message the application object from DynamoDB by app domain ID.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
+			if err != nil {
+				return err
+			}
+			id := flgs.ID
+			retrieved, err := client.GetMessage(ctx, &dynamomq.GetMessageInput{
+				ID: id,
+			})
+			if err != nil {
+				return err
+			}
+			printMessageWithData("", retrieved.Message)
+			return nil
+		},
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(getCmd)
-	getCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	getCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	getCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
-	getCmd.Flags().StringVar(&flgs.ID, flagMap.ID.Name, flagMap.ID.Value, flagMap.ID.Usage)
+	c := defaultCommandFactory.CreateGetCommand(flgs)
+	setDefaultFlags(c, flgs)
+	c.Flags().StringVar(&flgs.ID, flagMap.ID.Name, flagMap.ID.Value, flagMap.ID.Usage)
+	rootCmd.AddCommand(c)
 }
