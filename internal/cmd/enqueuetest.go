82a2437feeed842b1446f2ca56db727e00ba10f7
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:43:46 +0900

    refactor: refactor enqueue-test command to use CommandFactory

--- internal/cmd/enqueuetest.go
+++ internal/cmd/enqueuetest.go
@@ -3,73 +3,52 @@ package cmd
 import (
 	"context"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/spf13/cobra"
 	"github.com/vvatanabe/dynamomq"
-	"github.com/vvatanabe/dynamomq/internal/clock"
 	"github.com/vvatanabe/dynamomq/internal/test"
 )
 
-var enqueueTestCmd = &cobra.Command{
-	Use:   "enqueue-test",
-	Short: "Send test messages in DynamoDB table: A-101, A-202, A-303 and A-404; if already exists, it will overwrite it",
-	Long:  `Send test messages in DynamoDB table: A-101, A-202, A-303 and A-404; if already exists, it will overwrite it.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			printError(err)
-			return
-		}
-		var result EnqueueTestResult
-		ids := []string{"A-101", "A-202", "A-303", "A-404"}
-		for _, id := range ids {
-			message := dynamomq.NewMessage[test.MessageData](id, test.NewMessageData(id), clock.Now())
-			item, err := message.MarshalMap()
+func (f CommandFactory) CreateEnqueueTestCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "enqueue-test",
+		Short: "Send test messages in DynamoDB table: A-101, A-202, A-303 and A-404; if already exists, it will overwrite it",
+		Long:  `Send test messages in DynamoDB table: A-101, A-202, A-303 and A-404; if already exists, it will overwrite it.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
 			if err != nil {
-				result.Failures = append(result.Failures, Failure{
-					ID:    message.ID,
-					Error: err,
-				})
-				continue
+				return err
 			}
-			_, err = client.DeleteMessage(ctx, &dynamomq.DeleteMessageInput{
-				ID: id,
-			})
-			if err != nil {
-				result.Failures = append(result.Failures, Failure{
-					ID:    message.ID,
-					Error: err,
+			var result EnqueueTestResult
+			defer printMessageWithData("", result)
+			for _, id := range []string{"A-101", "A-202", "A-303", "A-404"} {
+				_, err = client.DeleteMessage(ctx, &dynamomq.DeleteMessageInput{
+					ID: id,
 				})
-				continue
-			}
-			_, err = client.GetDynamodbClient().PutItem(ctx, &dynamodb.PutItemInput{
-				TableName: aws.String(flgs.TableName),
-				Item:      item,
-			})
-			if err != nil {
-				result.Failures = append(result.Failures, Failure{
-					ID:    message.ID,
-					Error: err,
+				if err != nil {
+					return err
+				}
+				_, err = client.SendMessage(ctx, &dynamomq.SendMessageInput[any]{
+					ID:   id,
+					Data: test.NewMessageData(id),
 				})
-				continue
+				if err != nil {
+					return err
+				}
+				result.Successes = append(result.Successes, id)
 			}
-			result.Successes = append(result.Successes, message.ID)
-		}
-		printMessageWithData("", result)
-		return
-	},
+			return nil
+		},
+	}
 }
 
 type EnqueueTestResult struct {
-	Successes []string  `json:"successes"`
-	Failures  []Failure `json:"failures"`
+	Successes []string `json:"successes"`
 }
 
 func init() {
-	rootCmd.AddCommand(enqueueTestCmd)
-	enqueueTestCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	enqueueTestCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	enqueueTestCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
+	c := defaultCommandFactory.CreateEnqueueTestCommand(flgs)
+	setDefaultFlags(c, flgs)
+	c.Flags().StringVar(&flgs.ID, flagMap.ID.Name, flagMap.ID.Value, flagMap.ID.Usage)
+	rootCmd.AddCommand(c)
 }
