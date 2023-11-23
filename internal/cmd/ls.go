40fd242299a8a575782cd857747a0ed1527195dd
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:42:28 +0900

    refactor: refactor ls command to use CommandFactory

--- internal/cmd/ls.go
+++ internal/cmd/ls.go
@@ -7,33 +7,33 @@ import (
 	"github.com/vvatanabe/dynamomq"
 )
 
-var lsCmd = &cobra.Command{
-	Use:   "ls",
-	Short: "List all message IDs ... max 10 elements",
-	Long:  `List all message IDs ... max 10 elements.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			printError(err)
-			return
-		}
-		out, err := client.ListMessages(ctx, &dynamomq.ListMessagesInput{Size: 10})
-		if err != nil {
-			printError(err)
-			return
-		}
-		var result LSResult
-		for _, m := range out.Messages {
-			result.Statuses = append(result.Statuses, Status{
-				ID:        m.ID,
-				Status:    m.Status,
-				QueueType: m.QueueType,
-			})
-		}
-		printMessageWithData("", result)
-		return
-	},
+func (f CommandFactory) CreateLSCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "ls",
+		Short: "List all message IDs ... max 10 elements",
+		Long:  `List all message IDs ... max 10 elements.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
+			if err != nil {
+				return err
+			}
+			out, err := client.ListMessages(ctx, &dynamomq.ListMessagesInput{Size: 10})
+			if err != nil {
+				return err
+			}
+			var result LSResult
+			for _, m := range out.Messages {
+				result.Statuses = append(result.Statuses, Status{
+					ID:        m.ID,
+					Status:    m.Status,
+					QueueType: m.QueueType,
+				})
+			}
+			printMessageWithData("", result)
+			return nil
+		},
+	}
 }
 
 type LSResult struct {
@@ -47,8 +47,7 @@ type Status struct {
 }
 
 func init() {
-	rootCmd.AddCommand(lsCmd)
-	lsCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	lsCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	lsCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
+	c := defaultCommandFactory.CreateLSCommand(flgs)
+	setDefaultFlags(c, flgs)
+	rootCmd.AddCommand(c)
 }
