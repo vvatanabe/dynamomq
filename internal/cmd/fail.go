66135639d97010076f61c8b8be55a0a9a2b6f2b4
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-11-23 17:41:37 +0900

    refactor: refactor fail command to use CommandFactory

--- internal/cmd/fail.go
+++ internal/cmd/fail.go
@@ -8,48 +8,39 @@ import (
 	"github.com/vvatanabe/dynamomq"
 )
 
-var failCmd = &cobra.Command{
-	Use:   "fail",
-	Short: "Simulate failed message's processing ... put back to the queue; needs to be receive again",
-	Long:  `Simulate failed message's processing ... put back to the queue; needs to be receive again.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		client, _, err := createDynamoMQClient[any](ctx, flgs)
-		if err != nil {
-			printError(err)
-			return
-		}
-		id := flgs.ID
-		_, err = client.UpdateMessageAsVisible(ctx, &dynamomq.UpdateMessageAsVisibleInput{
-			ID: id,
-		})
-		if err != nil {
-			printError(err)
-			return
-		}
-		retrieved, err := client.GetMessage(ctx, &dynamomq.GetMessageInput{ID: id})
-		if err != nil {
-			printError(err)
-			return
-		}
-		if retrieved.Message == nil {
-			printError(fmt.Sprintf("Message's [%s] not found!", id))
-			return
-		}
-		message := retrieved.Message
-		printMessageWithData("", FailResult[any]{Message: message})
-		return
-	},
-}
-
-type FailResult[T any] struct {
-	Message *dynamomq.Message[T] `json:"message"`
+func (f CommandFactory) CreateFailCommand(flgs *Flags) *cobra.Command {
+	return &cobra.Command{
+		Use:   "fail",
+		Short: "Simulate failed message's processing ... put back to the queue; needs to be receive again",
+		Long:  `Simulate failed message's processing ... put back to the queue; needs to be receive again.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, _, err := f.CreateDynamoMQClient(ctx, flgs)
+			if err != nil {
+				return err
+			}
+			_, err = client.UpdateMessageAsVisible(ctx, &dynamomq.UpdateMessageAsVisibleInput{
+				ID: flgs.ID,
+			})
+			if err != nil {
+				return err
+			}
+			retrieved, err := client.GetMessage(ctx, &dynamomq.GetMessageInput{ID: flgs.ID})
+			if err != nil {
+				return err
+			}
+			if retrieved.Message == nil {
+				return fmt.Errorf("message's [%s] not found", flgs.ID)
+			}
+			printMessageWithData("", retrieved.Message)
+			return nil
+		},
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(failCmd)
-	failCmd.Flags().StringVar(&flgs.TableName, flagMap.TableName.Name, flagMap.TableName.Value, flagMap.TableName.Usage)
-	failCmd.Flags().StringVar(&flgs.QueueingIndexName, flagMap.QueueingIndexName.Name, flagMap.QueueingIndexName.Value, flagMap.QueueingIndexName.Usage)
-	failCmd.Flags().StringVar(&flgs.EndpointURL, flagMap.EndpointURL.Name, flagMap.EndpointURL.Value, flagMap.EndpointURL.Usage)
-	failCmd.Flags().StringVar(&flgs.ID, flagMap.ID.Name, flagMap.ID.Value, flagMap.ID.Usage)
+	c := defaultCommandFactory.CreateFailCommand(flgs)
+	setDefaultFlags(c, flgs)
+	c.Flags().StringVar(&flgs.ID, flagMap.ID.Name, flagMap.ID.Value, flagMap.ID.Usage)
+	rootCmd.AddCommand(c)
 }
